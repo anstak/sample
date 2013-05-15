3bf4d4b01e8cf45ae603ca58ade8844e8a35f4a4
anstak <anstak@gmail.com>
2013-05-15 10:37:01 +0300

    Finished chapter 5

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  let(:defalut_title) {"Ruby on Rails Tutorial Sample App"}
+  subject { page }
+
+  describe "Home page" do
+    before { visit root_path }
+
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector 'title', text: defalut_title }
+    it { should_not have_selector 'title', text: '| Home' }
+  end
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector("h1",text:'Help page') }
+    it { should have_selector('title', :text => "#{defalut_title} | Help") }
+  end
+  describe "About us" do
+    before { visit about_path }
+
+    it { should have_selector("h1",text:'About us') }
+    it { should have_selector('title', :text => "#{defalut_title} | About") }
+  end
+  describe "Contact us" do
+    before { visit contact_path }
+
+    it { should have_selector("h1",text:'Contact us') }
+    it { should have_selector('title', :text => "#{defalut_title} | Contact") }
+  end
+end
